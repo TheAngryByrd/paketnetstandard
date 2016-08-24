bd2b4b28000a09b183604cdd4721652f997491b9
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-08-24 16:28:41 -0400

    Build with nuget, not with paket

--- vs/ConsoleApplication1/ConsoleApplication1/Program.fs
+++ vs/ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -1,7 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Marten
+open System
+open System.Linq
+
+[<CLIMutable>]
+type User = {
+    Id : Guid
+    Name : string
+}
 
 [<EntryPoint>]
 let main argv = 
+    let store = DocumentStore.For("host=localhost;database=marten_test;password=mypassword;username=someuser");
+    use session = store.LightweightSession()
+    let user = { Id = Guid.NewGuid() ; Name = "Darth Vader"}
+    session.Store(user)
+    session.SaveChanges()
+    let queryUser =session
+                    .Query<User>()
+                    .Where(fun x -> x.Name = "Darth Vader")
+                    .Single();
     printfn "%A" argv
     0 // return an integer exit code
--- vscode/src/vscode/vscode.fs
+++ vscode/src/vscode/vscode.fs
@@ -1,6 +1,23 @@
-module vscode
+open Marten
+open System
+open System.Linq
+
+[<CLIMutable>]
+type User = {
+    Id : Guid
+    Name : string
+}
 
 [<EntryPoint>]
-let main argv =
+let main argv = 
+    let store = DocumentStore.For("host=localhost;database=marten_test;password=mypassword;username=someuser");
+    use session = store.LightweightSession()
+    let user = { Id = Guid.NewGuid() ; Name = "Darth Vader"}
+    session.Store(user)
+    session.SaveChanges()
+    let queryUser =session
+                    .Query<User>()
+                    .Where(fun x -> x.Name = "Darth Vader")
+                    .Single();
     printfn "%A" argv
     0 // return an integer exit code
